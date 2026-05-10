271a5e09e5f642cb8d1707c5c14c4f7989e80848
Sebastien Galvagno <dev@igalvagno.info>
2026-05-10 19:14:54 +0200

    added wget bottle

---
+++ Formula/wget.rb
@@ -0,0 +1,52 @@
+class Wget < Formula
+  desc "Internet file retriever"
+  homepage "https://www.gnu.org/software/wget/"
+  url "https://ftpmirror.gnu.org/gnu/wget/wget-1.25.0.tar.gz"
+  sha256 "766e48423e79359ea31e41db9e5c289675947a7fcf2efdcedb726ac9d0da3784"
+  license "GPL-3.0-or-later"
+
+  no_autobump! because: :requires_manual_review
+
+  bottle do
+    sha256 ventura:       "78cee523a9b58a7b824b51767935f68c9838e9f673e70d001982858001e766ff"
+  end
+
+  head do
+    url "https://git.savannah.gnu.org/git/wget.git", branch: "master"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "xz" => :build
+  end
+
+  depends_on "pkgconf" => :build
+  depends_on "libidn2"
+  depends_on "openssl@3"
+
+  on_macos do
+    depends_on "gettext"
+    depends_on "libunistring"
+  end
+
+  on_linux do
+    depends_on "util-linux"
+    depends_on "zlib-ng-compat"
+  end
+
+  def install
+    system "./bootstrap", "--skip-po" if build.head?
+    system "./configure", "--prefix=#{prefix}",
+                          "--sysconfdir=#{etc}",
+                          "--with-ssl=openssl",
+                          "--with-libssl-prefix=#{Formula["openssl@3"].opt_prefix}",
+                          "--disable-pcre",
+                          "--disable-pcre2",
+                          "--without-libpsl",
+                          "--without-included-regex"
+    system "make", "install"
+  end
+
+  test do
+    system bin/"wget", "-O", File::NULL, "https://google.com"
+  end
+end
